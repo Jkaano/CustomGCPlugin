dddb641a5c6f9128503e3e29ab657fb5a69d91ee
Jkaano <jkaano@gmail.com>
2017-01-09 19:22:40 -0600

    Detects if Egg on ground
    Can detect if an egg is on the ground and near a chicken, then changes
    to diamond axe for now. I need to detect if the egg is within a very
    small area of the chicken when dropped(Under 1 block) and then if
    anyothers are around.

--- src/gc/customplugin/events/LayEggEvent.java
+++ src/gc/customplugin/events/LayEggEvent.java
@@ -1,23 +1,41 @@
 package gc.customplugin.events;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Chicken;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ItemSpawnEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class LayEggEvent implements Listener{
-	
-	private Entity ent;
 	
 	@EventHandler
 	public void onLayEgg(ItemSpawnEvent e){
 		
-		ent = e.getEntity();
+		Item item = e.getEntity();
+		ItemStack stack = item.getItemStack();
+		Material mat = stack.getType();
 		
-	}
-	
-	public Entity getEnt(){
-		return ent;
+		if(mat == Material.EGG){
+
+			List<Entity> nearbyEnt = new ArrayList<Entity>();
+			
+			nearbyEnt.addAll(item.getNearbyEntities(10.0, 10.0, 10.0));
+			
+			for(int i = 0; i < nearbyEnt.size(); i++){
+				
+				if(nearbyEnt.get(i) instanceof Chicken){
+					stack.setType(Material.DIAMOND_AXE);
+				}
+				
+			}
+		}
+		
 	}
 	
 }
